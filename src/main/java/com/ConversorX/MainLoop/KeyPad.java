4fbcceb95b635afd7f58668ecff361e15d99850e
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-06-30 23:23:25 -0500

    tiene 2 metodos 1 para la entrada tipo int de las opciones y double para los montos del dinero

--- src/main/java/com/ConversorX/MainLoop/KeyPad.java
+++ src/main/java/com/ConversorX/MainLoop/KeyPad.java
@@ -1,4 +1,33 @@
 package com.ConversorX.MainLoop;
 
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class KeyPad {
+    private Scanner input;
+
+    public KeyPad() {
+        input = new Scanner(System.in);
+    }
+    public int getInputInt() {
+        try {
+            return input.nextInt();
+        } catch (NoSuchElementException e) {
+            System.out.println("La entrada debe ser un entero valido");
+            System.out.println("Vuelva a intentarlo");
+            input.nextLine();
+            return -1;
+        }
+    }
+
+    public double getInputDouble() {
+        try {
+            return input.nextDouble();
+        } catch (NoSuchElementException e) {
+            System.out.println("La entrada debe ser un entero valido");
+            System.out.println("Vuelva a intentarlo");
+            input.nextLine();
+            return -1;
+        }
+    }
 }
