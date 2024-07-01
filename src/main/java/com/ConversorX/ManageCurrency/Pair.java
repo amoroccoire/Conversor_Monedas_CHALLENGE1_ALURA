df66246a76bd418355a7b8fc09b1bbc15f63785b
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-06-30 23:25:16 -0500

    Esta clase asocia el identificador de la moneda como PEN o ARG y el nombre de la moneda

--- src/main/java/com/ConversorX/ManageCurrency/Pair.java
+++ src/main/java/com/ConversorX/ManageCurrency/Pair.java
@@ -1,4 +1,26 @@
 package com.ConversorX.ManageCurrency;
 
 public class Pair {
+    private String currencyId;
+    private String name;
+
+    public Pair(String currencyId, String name) {
+        this.currencyId = currencyId;
+        this.name = name;
+    }
+
+    public void setCurrencyId(String currencyId) {
+        this.currencyId = currencyId;
+    }
+    public String getCurrencyId() {
+        return this.currencyId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 }
